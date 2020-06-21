bcb1d6a406092873404b00cb7bbccc03f1ee234e
Soleil Oliva <soleiloliva@gmail.com>
2020-06-21 20:47:21 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -5,12 +5,13 @@ function basicTeenager(age) {
     }
 
 
-
-
-
 }
 
 function teenager(age) {
+  if (age >= 13 && age <= 19) {
+    return "You are a teenager!";
+  } else { 
+  return "You are not a teenager!";}
 
 }
 
