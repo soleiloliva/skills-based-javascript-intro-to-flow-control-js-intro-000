64b2bb80fe8c771a17da7aaf2f64c238073d26df
Soleil Oliva <soleiloliva@gmail.com>
2020-06-21 20:58:40 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -19,12 +19,12 @@ function ageChecker(age) {
   if (age >= 13 && age <= 19) {
     return "You are a teenager!";
   } else if (age <= 12) {
-    return "You are a kid"; 
+    return "You are a kid";
   } else {
     if (age > 19) {
     return "You are a grownup"; }
     }
-    
+
   }
 
 }
